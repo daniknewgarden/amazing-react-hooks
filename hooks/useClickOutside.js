1227837b4b1e3a87e28777518c73e59263eb2fc8
Danik <daniknewgarden@gmail.com>
2021-01-18 18:31:54 +0200

    Added js destructuring to useClickOutside hook arguments

--- hooks/useClickOutside.js
+++ hooks/useClickOutside.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export const useClickOutside = (ref, callback) => {
+export const useClickOutside = ({ ref, callback, enabled = true }) => {
   const handleClick = (e) => {
     if (ref.current && !ref.current.contains(e.target)) {
       callback();
